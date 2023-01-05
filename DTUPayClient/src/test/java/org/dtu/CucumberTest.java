476b936f1e7cb29e17cc5cdee15eca688cf6e0c3
s184209 <s184209@student.dtu.dk>
2023-01-05 12:14:17 +0100

    added plugin format

--- DTUPayClient/src/test/java/org/dtu/CucumberTest.java
+++ DTUPayClient/src/test/java/org/dtu/CucumberTest.java
@@ -4,10 +4,10 @@ import org.junit.runner.RunWith;
 
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
-import io.cucumber.junit.CucumberOptions.SnippetType;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
+        plugin = {"json:target/cucumber.json"},
         features = "features",// directory of the feature files
         glue = "org.dtu"
 )
