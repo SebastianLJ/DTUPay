5b56ac172dd052b770c4aa25e767e97a9c061903
jayveedee <jayveedees@gmail.com>
2023-01-09 10:25:29 +0100

    edited docker files

--- build.sh
+++ build.sh
@@ -1,22 +1,20 @@
-#!/bin/cmd
+#!/bin/bash
 set -e
 
-#
+# Clean old builds
 pushd DTUPayWebService
+mvn clean package
 
-#
-mvn package
-
-#
+# Build the docker image and run it
 popd
 docker-compose build
 docker-compose up -d
 docker image prune -f
 
-
 # Wait for web server to run
-sleep 3
+sleep 2
 
+# Run the client tests against the server
 pushd DTUPayClient
 mvn test
 popd
