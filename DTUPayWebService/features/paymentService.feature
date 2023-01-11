c9983bf172c2c8f5a06a3f62761f96a45cf455b5
s184209 <s184209@student.dtu.dk>
2023-01-11 09:54:29 +0100

    added more tests

--- DTUPayWebService/features/paymentService.feature
+++ DTUPayWebService/features/paymentService.feature
@@ -5,3 +5,12 @@ Feature: Payment Service
     When the merchant creates a payment
     And the customer approves it
     Then a payment is registered
+
+  Scenario: A payment is successfully found
+    Given there exists a payment
+    When the id of the payment is queried
+    Then the payment is found
+
+  Scenario: Payment does not exists
+    Given the id of a new payment is queried
+    Then the payment is not found
--- DTUPayWebService/src/test/java/org/dtu/PaymentServiceSteps.java
+++ DTUPayWebService/src/test/java/org/dtu/PaymentServiceSteps.java
@@ -9,7 +9,10 @@ import org.dtu.aggregate.Payment;
 import org.dtu.aggregate.User;
 import org.dtu.factories.PaymentFactory;
 import org.dtu.services.PaymentService;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class PaymentServiceSteps {
     PaymentService paymentRegistration = new PaymentFactory().getService();
@@ -19,6 +22,7 @@ public class PaymentServiceSteps {
     User customer = null;
     User merchant = null;
     Payment payment = null;
+    Payment result = null;
 
     @Given("^there is a registered customer$")
     public void thereIsARegisteredCustomer() {
@@ -59,7 +63,46 @@ public class PaymentServiceSteps {
         try {
             assertNotNull(paymentRegistration.getPayment(payment.getId()));
         } catch (PaymentNotFoundException e) {
-            e.printStackTrace();
+            //fail if payment is not found
+            fail();
+        }
+    }
+
+    @Given("there exists a payment")
+    public void thereExistsAPayment() {
+        this.thereIsARegisteredCustomer();
+        this.thereIsARegisteredMerchant();
+        this.theMerchantCreatesAPayment();
+        this.theCustomerApprovesIt();
+        this.aPaymentIsRegistered();
+    }
+
+    @When("the id of the payment is queried")
+    public void theIdOfThePaymentIsQueried() {
+        try {
+            result = paymentRegistration.getPayment(payment.getId());
+        } catch (PaymentNotFoundException e) {
+            result = null;
+        }
+    }
+
+    @Then("the payment is found")
+    public void thePaymentIsFound() {
+        assertEquals(payment.getId(), result.getId());
+    }
+
+    @Then("the payment is not found")
+    public void thePaymentIsNotFound() {
+        assertNull(result);
+    }
+
+    @Given("the id of a new payment is queried")
+    public void theIdOfANewPaymentIsQueried() {
+        try {
+            result = paymentRegistration.getPayment(UUID.randomUUID());
+        } catch (PaymentNotFoundException e) {
+            //if payment is not found set result to null
+            result = null;
         }
     }
 }
