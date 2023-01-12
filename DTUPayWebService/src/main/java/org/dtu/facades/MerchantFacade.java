bb1dabee6fcd8d687bd795482a211222a098ebff
s205347 <s205347@student.dtu.dk>
2023-01-12 10:32:47 +0100

    Merchant register added to MerchantFacade

--- DTUPayWebService/src/main/java/org/dtu/facades/MerchantFacade.java
+++ DTUPayWebService/src/main/java/org/dtu/facades/MerchantFacade.java
@@ -1,4 +1,36 @@
 package org.dtu.facades;
 
+import org.dtu.aggregate.User;
+import org.dtu.exceptions.MerchantAlreadyExistsException;
+import org.dtu.factories.MerchantFactory;
+import org.dtu.services.MerchantService;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+@Path("/merchants")
 public class MerchantFacade {
+    MerchantService merchantRegistration = new MerchantFactory().getService();
+
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Path("/register")
+    public Response registerMerchant(String firstName, String lastName) {
+        try {
+            User newUser = merchantRegistration.addMerchant(firstName, lastName);
+            return Response.status(Response.Status.CREATED)
+                    .entity("merchant with id " + newUser.getUserId().getUuid() + " created")
+                    .build();
+        } catch (MerchantAlreadyExistsException e) {
+            return Response.status(Response.Status.CONFLICT)
+                    .entity("merchant with the same id already exists")
+                    .build();
+        }
+    }
+
+
 }
