6a975f23b2b867e9a41f66cd280e736b4ee7b516
alexfc97 <alexfc@live.dk>
2023-01-12 15:29:14 +0100

    Changes to Token management

--- DTUPayTokenManagement/src/main/java/org/dtu/repository/TokenRepository.java
+++ DTUPayTokenManagement/src/main/java/org/dtu/repository/TokenRepository.java
@@ -2,6 +2,7 @@ package org.dtu.repository;
 
 import org.dtu.aggregate.Token;
 import org.dtu.aggregate.UserId;
+import org.dtu.exceptions.InvalidTokenAmountException;
 import org.dtu.exceptions.TokenDoesNotExistException;
 import org.dtu.exceptions.TokenHasAlreadyBeenUsedException;
 
@@ -10,15 +11,19 @@ import java.util.*;
 public class TokenRepository {
 
     private HashMap<Token, UserId> tokenRepository = new HashMap<>();
+    private HashMap<UserId, Integer> tokenAmountRepository = new HashMap<UserId, Integer>();
     private HashMap<Token, UserId> usedTokenRepository = new HashMap<>();
 
-    public ArrayList<Token> generateTokens(UserId userid, int amount) {
+    public ArrayList<Token> generateTokens(UserId userid, int amount) throws InvalidTokenAmountException {
+        int tokensAmount = tokenAmountRepository.get(userid);
+        if (tokensAmount > 1 || tokensAmount < 0) throw new InvalidTokenAmountException();
         ArrayList<Token> tokens = new ArrayList<>();
         for (int i = 0 ; i < amount ; i++){
             Token token = new Token();
             tokenRepository.put(token,userid);
             tokens.add(token);
         }
+        tokenAmountRepository.put(userid, tokens.size());
         return tokens;
     }
 
@@ -28,6 +33,7 @@ public class TokenRepository {
         if (user == null) throw new TokenDoesNotExistException();
         usedTokenRepository.put(token, user);
         tokenRepository.remove(token);
+        tokenAmountRepository.put(user, tokenAmountRepository.get(user) - 1);
         return user;
     }
 
