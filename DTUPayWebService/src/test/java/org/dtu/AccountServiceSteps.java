5c8075b3ce70b2a8d3fc8c13a9b4d47acf811dbf
Sebastian <juste99@live.dk>
2023-01-12 13:05:17 +0100

    merchant customer test

--- DTUPayWebService/src/test/java/org/dtu/AccountServiceSteps.java
+++ DTUPayWebService/src/test/java/org/dtu/AccountServiceSteps.java
@@ -1,19 +1,22 @@
 package org.dtu;
 
 import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.dtu.aggregate.User;
-import org.dtu.exceptions.InvalidMerchantIdException;
-import org.dtu.exceptions.MerchantAlreadyExistsException;
+import org.dtu.exceptions.*;
 import org.dtu.factories.MerchantFactory;
 import org.dtu.services.MerchantService;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 public class AccountServiceSteps {
     MerchantService merchantService = new MerchantFactory().getService();
     User merchant;
+
+    //Create merchant scenario
     @When("a merchant is created")
     public void aMerchantIsCreated() throws MerchantAlreadyExistsException {
         merchant = merchantService.addMerchant("John", "Doe");
@@ -33,5 +36,19 @@ public class AccountServiceSteps {
         } catch (InvalidMerchantIdException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    //Delete merchant scenario
+    @Given("a merchant is in the system")
+    public void a_merchant_is_in_the_system() throws MerchantAlreadyExistsException {
+        merchant = merchantService.addMerchant("Martin", "Nielsen");
+    }
+    @When("a merchant is deleted")
+    public void a_merchant_is_deleted() throws InvalidMerchantIdException, MerchantNotFoundException, PaymentNotFoundException {
+        merchantService.deleteMerchant(merchant.getUserId().getUuid());
+    }
+    @Then("the merchant cannot be found")
+    public void the_merchant_cannot_be_found() throws InvalidMerchantIdException {
+        assertNull(merchantService.getMerchant(merchant.getUserId().getUuid()));
     }
 }
