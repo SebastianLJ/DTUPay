b7b5fe6676a26a4c1dd219e5cd2222a8c0aa29c3
s205347 <s205347@student.dtu.dk>
2023-01-13 10:23:20 +0100

    Correction

--- DTUPayClient/src/test/java/org/dtu/CustomerSteps.java
+++ DTUPayClient/src/test/java/org/dtu/CustomerSteps.java
@@ -10,7 +10,7 @@ import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-public class Customer_2Steps {
+public class CustomerSteps {
     SimpleDTUPay dtuPay = new SimpleDTUPay();
     CustomerApp customerApp = new CustomerApp();
 
