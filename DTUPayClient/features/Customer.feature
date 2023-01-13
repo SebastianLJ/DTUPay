8880d12ff8467d8d57002fdd8806bc3fa02431df
s205347 <s205347@student.dtu.dk>
2023-01-13 10:27:01 +0100

    New customer scenario testing added

--- DTUPayClient/features/Customer.feature
+++ DTUPayClient/features/Customer.feature
@@ -4,3 +4,9 @@ Feature: Customer tests
     Given a customer is in the system
     When the customer requests 3 tokens
     Then the customer receives 3 tokens
+
+    Scenario: A customer is successfully deleted
+      Given a customer is already in the system
+      When a customer is deleted
+      Then the customer is no longer in the system
+      And the customers tokens have been deleted
--- DTUPayClient/src/test/java/org/dtu/CustomerSteps.java
+++ DTUPayClient/src/test/java/org/dtu/CustomerSteps.java
@@ -3,6 +3,7 @@ package org.dtu;
 import aggregate.User;
 import aggregate.Token;
 import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
@@ -39,6 +40,29 @@ public class CustomerSteps {
     public void theCustomerReceivesTokens(int amount) {
         assertEquals(amount, tokens.size());
     }
+
+
+    @Given("a customer is already in the system")
+    public void a_customer_is_already_in_the_system() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @When("a customer is deleted")
+    public void a_customer_is_deleted() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("the customer is no longer in the system")
+    public void the_customer_is_no_longer_in_the_system() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("the customers tokens have been deleted")
+    public void the_customers_tokens_have_been_deleted() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
 
 
 }
