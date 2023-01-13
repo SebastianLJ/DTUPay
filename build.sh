8698fb6f5f7a46e932eabda277e2269a504a62b2
jayveedee <jayveedees@gmail.com>
2023-01-13 06:52:29 +0100

    Remove some verbose logging from build script (only show errors) and added some step indication in the output

--- build.sh
+++ build.sh
@@ -2,41 +2,42 @@
 # Enforces build script fails on anything but SUCCESS codes
 set -e
 
-docker-compose up -d rabbitMq
-
 # Clean and build projects
+printf "\033[94mStep 1/4: Clean and build projects\033[0m\n"
 pushd DTUPayMessageQueue
-mvn clean install
+mvn clean install -q -DskipTests
 popd
 
 pushd DTUPayWebService
-mvn package
+mvn clean package -q -DskipTests
 popd
 
-# Prune unused docker images
+# Prune & build the docker images and run them
+printf "\n\033[94mStep 2/4: Prune and build the docker images and run them\033[0m\n"
 docker image prune -f
-
-# Build the docker images and run them
-docker-compose up -d DTUPayWebService
+docker-compose up -d
 
 # Wait for any builds to run
 sleep 2
 
 # Run project tests
+printf "\n\033[94mStep 3/4: Run project tests\033[0m\n"
 pushd DTUPayClient
-mvn test
+mvn test -q
 popd
 
 pushd DTUPayTokenManagement
-mvn test
+mvn test -q
 popd
 
 pushd DTUPayMessageQueue
-mvn test
+mvn test -q
 popd
 
 pushd DTUPayWebService
-mvn test
+mvn test -q
 popd
 
+# Clean docker images
+printf "\n\033[94mStep 4/4: Clean docker images\033[0m\n"
 ./clean-docker.sh
