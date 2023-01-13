6555e82f3ec75c60d649a9bbd73b2d45c696f1a8
s205347 <s205347@student.dtu.dk>
2023-01-13 10:22:01 +0100

    Customer end-to-end testing started

--- DTUPayClient/src/test/java/org/dtu/Customer_2Steps.java
+++ DTUPayClient/src/test/java/org/dtu/Customer_2Steps.java
@@ -2,19 +2,15 @@ package org.dtu;
 
 import aggregate.User;
 import aggregate.Token;
-import dtu.ws.fastmoney.BankServiceException_Exception;
 import io.cucumber.java.en.And;
-import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
-import java.math.BigDecimal;
-import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-public class CustomerSteps {
+public class Customer_2Steps {
     SimpleDTUPay dtuPay = new SimpleDTUPay();
     CustomerApp customerApp = new CustomerApp();
 
