66380b3d8bc5a2703f7e8ce0fcec0cb5d5f288e4
alexfc97 <alexfc@live.dk>
2023-01-18 14:26:36 +0100

    new quarkus funct

--- DTUPayTokenManagementTest/src/main/java/org/dtu/StartUp.java
+++ DTUPayTokenManagementTest/src/main/java/org/dtu/StartUp.java
@@ -1,13 +1,27 @@
 package org.dtu;
 
+import io.quarkus.runtime.Quarkus;
+import io.quarkus.runtime.QuarkusApplication;
+import io.quarkus.runtime.Startup;
+import io.quarkus.runtime.annotations.QuarkusMain;
 import org.dtu.factories.TokenFactory;
 
+@QuarkusMain
 public class StartUp {
-    public static void main(String[] args) {
-        new StartUp().startTokenManagement();
+    public static void main(String... args) {
+        Quarkus.run(MyApp.class, args);
     }
-
     private void startTokenManagement() {
         new TokenFactory().getService();
+    }
+
+    public static class MyApp implements QuarkusApplication {
+
+        @Override
+        public int run(String... args) throws Exception {
+            new StartUp().startTokenManagement();
+            Quarkus.waitForExit();
+            return 0;
+        }
     }
 }
