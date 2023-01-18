11eb3a61ef4e2e35ceecd2c8265a82eed8bb0c93
s184209 <s184209@student.dtu.dk>
2023-01-18 13:58:18 +0100

    added payment list to full report generated

--- DTUPayWebService/src/main/java/org/dtu/events/FullReportGenerated.java
+++ DTUPayWebService/src/main/java/org/dtu/events/FullReportGenerated.java
@@ -4,11 +4,15 @@ import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Value;
 import messageUtilities.cqrs.events.Event;
+import org.dtu.aggregate.Payment;
 import org.dtu.aggregate.UserId;
+
+import java.util.List;
 
 @Value
 @AllArgsConstructor
 @EqualsAndHashCode(callSuper = false)
 public class FullReportGenerated extends Event {
     private static final long serialVersionUID = -7558442089500047644L;
+    List<Payment> payments;
 }
