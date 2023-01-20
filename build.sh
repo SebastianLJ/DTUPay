94d3cf53e9444b96a2787349ff83117022749a21
alexfc97 <alexfc@live.dk>
2023-01-20 19:05:19 +0100

    changed sleep

--- build.sh
+++ build.sh
@@ -33,11 +33,11 @@ docker image prune -f
 # @author Jákup Viljam Dam - s185095
 printf "\n\033[94mStep 3/5: build the docker images and run them\033[0m\n"
 docker-compose up -d rabbitMq
-sleep 20
+sleep 10
 docker-compose up -d
 
 # Wait for any builds to run
-sleep 10
+sleep 5
 
 # Run project tests
 
